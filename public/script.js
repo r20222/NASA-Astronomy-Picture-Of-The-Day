089192055aae200613eddece08933d656039beb5
r20222 <101579892+r20222@users.noreply.github.com>
2023-07-20 23:14:51 +0200

    column reverse messages

--- public/script.js
+++ public/script.js
@@ -47,3 +47,19 @@ function elementSupportsAttribute(attribute) {
   })
   
 };
+
+
+// prevent multiple submissions
+// bron: https://www.bram.us/2020/11/04/preventing-double-form-submissions/
+
+document.querySelectorAll('form').forEach(form => {
+	form.addEventListener('submit', (e) => {
+		// Prevent if already submitting
+		if (form.classList.contains('is-submitting')) {
+			e.preventDefault();
+		}
+		
+		// Add class to hook our visual indicator on
+		form.classList.add('is-submitting');
+	});
+});
