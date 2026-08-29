1c00d19587e19bd46f53924ff238d7d3ffbda558
r20222 <101579892+r20222@users.noreply.github.com>
2026-08-29 18:36:58 +0200

    🚧 wip error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<section>
+	<h1>This page is not found!</h1>
+	<p>Go back <a href="/" title="Go back to the homepage.">home</a>!</p>
+</section>
+
+<style>
+	section {
+		margin: 1rem;
+	}
+    a {
+		color: var(--dark-blue);
+		text-decoration: underline var(--dark-blue);
+
+		&:hover, &:focus {
+			color: var(--darker-blue);
+			text-decoration: underline var(--text);
+		}
+	}
+</style>
