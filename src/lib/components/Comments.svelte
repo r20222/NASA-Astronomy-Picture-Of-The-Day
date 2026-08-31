34ac248fb30dd8dedb4b621c90febc3c39756f05
r20222 <101579892+r20222@users.noreply.github.com>
2026-08-31 15:06:09 +0200

    💄 comments scroll

--- src/lib/components/Comments.svelte
+++ src/lib/components/Comments.svelte
@@ -5,51 +5,68 @@
 
 <section>
 	<h2>Comments:</h2>
-	{#if !hasMessage}
-		<p>No comments yet, be the first to leave a comment! 🛸</p>
-	{/if}
-	{#if hasMessage}
-		<ul>
-			{#each messages as message}
-				<!-- Check which comments belong to current apod photo -->
-				{#if apodDate === message.date}
-					<li>
-                        <h3>{message.name}<span>{message.today}</span></h3>
-                        <p>{message.message}</p>
-					</li>
-				{/if}
-			{/each}
-		</ul>
-	{/if}
+
+	<div>
+		{#if !hasMessage}
+			<p>No comments yet, be the first to leave a comment! 🛸</p>
+		{/if}
+		{#if hasMessage}
+			<ul>
+				{#each messages as message}
+					<!-- Check which comments belong to current apod photo -->
+					{#if apodDate === message.date}
+						<li>
+							<h3>{message.name}<span>{message.today}</span></h3>
+							<p>{message.message}</p>
+						</li>
+					{/if}
+				{/each}
+			</ul>
+		{/if}
+	</div>
 </section>
 
 <style>
 	section {
-		color: var(--text);
 		grid-area: comments;
+		height: 25rem;
 	}
-    h3 {
+	div {
+		position: relative;
+		margin-bottom: 3rem;
+		height: calc(100% - 4.5rem);
+		color: var(--text);
+		overflow: auto;
+	}
+
+	ul {
+		position: absolute;
+		height: 100%;
+		inset: 0;
+		padding-left: 0;
+		list-style: none;
+	}
+	li {
+		padding: 1rem;
+		margin: 0.5rem 0 0.5rem 0;
+		background-color: var(--not-white);
+		border: 1px solid var(--dark-blue);
+		border-radius: 1rem;
+	}
+	h3 {
 		margin: 0 0 auto 0;
 		display: flex;
 		justify-content: space-between;
-        gap:1rem;
-        & span {
-            font-size:1rem;
-            font-weight: 400;
-        }
+		gap: 1rem;
+		& span {
+			font-size: 1rem;
+			font-weight: 400;
+		}
 	}
-    ul {
-        position: relative;
-        padding-left:0; 
-        max-height:25rem;
-        overflow-y: scroll;
-        list-style: none;
-    }
-    li {
-        padding: 1rem;
-		margin: 0.5rem 0 0.5rem 0;
-		background-color: var(--not-white);
-        border: 1px solid var(--dark-blue);
-		border-radius: 1rem;
+	@media screen and (min-width: 48em) {
+		section {
+			grid-area: comments;
+			height: 100%;
+		}
 	}
 </style>
